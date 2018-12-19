cc83a22a3b7f298ebb4d90bf2860ad750315b6e9
Ekram Ul Ratul <45538412+Ekramm@users.noreply.github.com>
2018-12-20 02:27:37 +0600

    Update payment.php

--- admin/payment.php
+++ admin/payment.php
@@ -36,5 +36,20 @@ if(!isset($_SESSION["user"]))
                 </button>
                 <a class="navbar-brand" href="home.php"><?php echo $_SESSION["user"]; ?> </a>
             </div>
+		
+		 <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="usersetting.php"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="settings.php"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
       
 </html>
