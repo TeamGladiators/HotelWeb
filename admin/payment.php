8278c53146a22c1ce9c74a729eef9accd1f479f1
Ekram Ul Ratul <45538412+Ekramm@users.noreply.github.com>
2018-12-20 02:26:58 +0600

    Update payment.php

--- admin/payment.php
+++ admin/payment.php
@@ -26,6 +26,15 @@ if(!isset($_SESSION["user"]))
 </head>
 <body>
     <div id="wrapper">
-        
+        <nav class="navbar navbar-default top-navbar" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="home.php"><?php echo $_SESSION["user"]; ?> </a>
+            </div>
       
 </html>
