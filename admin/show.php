b937a63ae4d17411336aa3fdfb5039ec58a4dd9d
MD.Shamim Rahman <43651007+Shamimrahman@users.noreply.github.com>
2018-12-28 14:51:25 +0600

    Update show.php

--- admin/show.php
+++ admin/show.php
@@ -44,3 +44,11 @@ th, td { border-width: 1px; padding: 0.5em; position: relative; text-align: left
 th, td { border-radius: 0.25em; border-style: solid; }
 th { background: #EEE; border-color: #BBB; }
 td { border-color: #DDD; }
+			/* page */
+
+html { font: 16px/1 'Open Sans', sans-serif; overflow: auto; padding: 0.5in; }
+html { background: #999; cursor: default; }
+
+body { box-sizing: border-box; height: 11in; margin: 0 auto; overflow: hidden; padding: 0.5in; width: 8.5in; }
+body { background: #FFF; border-radius: 1px; box-shadow: 0 0 1in -0.25in rgba(0, 0, 0, 0.5); }
+
