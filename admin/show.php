f2093f35646619bb422ce4a2bfd4c0d0b522da99
Ekram Ul Ratul <45538412+Ekramm@users.noreply.github.com>
2018-12-28 15:01:37 +0600

    Update show.php

--- admin/show.php
+++ admin/show.php
@@ -258,3 +258,12 @@ tr:hover .cut { opacity: 1; }
 			</div>
 		</aside>
 	</body>
+	
+		</article>
+		<aside>
+			<h1><span >Contact us</span></h1>
+			<div >
+				<p align="center">Email :- info@sunrise.com || Web :- www.sunrise.com || Phone :- +94 65 222 44 55 </p>
+			</div>
+		</aside>
+	</body>
