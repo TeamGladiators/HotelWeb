f9a883cde60c8aaab02405ece6db221a11c701be
Talbia Kabir <talbia165@gmail.com>
2018-12-28 16:50:55 +0600

    Update profit.php

--- admin/profit.php
+++ admin/profit.php
@@ -244,3 +244,14 @@ else<td>$".$row['ttot']."</td>
     <script src="assets/js/bootstrap.min.js"></script>
     <!-- Metis Menu Js -->
     <script src="assets/js/jquery.metisMenu.js"></script>
+ <!-- DATA TABLE SCRIPTS -->
+    <script src="assets/js/dataTables/jquery.dataTables.js"></script>
+    <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+        <script>
+            $(document).ready(function () {
+                $('#dataTables-example').dataTable();
+            });
+    </script>
+         <!-- Custom Js -->
+    <script src="assets/js/custom-scripts.js"></script>
+    
